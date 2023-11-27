b5a85567b119433909dc542822f8b24b7983ee3d
DevExpressExampleBot <fakeEmail>
2023-11-27 20:45:33 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,62 +1,62 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
 Imports System.Collections.ObjectModel
-Imports System.Globalization
-Imports System.Windows.Data
-Imports System.Windows.Markup
 Imports System.Collections
 Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Core
 Imports System.Windows.Controls
 
 Namespace DXGridSample
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
-        Private Persons As ObservableCollection(Of Person)
+
+        Public Property Persons As ObservableCollection(Of Person)
+
+        Public Property Bands As List(Of BandItem)
+
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
             Persons = New ObservableCollection(Of Person)()
-            For i As Integer = 0 To 99
+            For i As Integer = 0 To 100 - 1
                 Persons.Add(New Person With {.Id = i, .Name = "Name" & i, .Bool = i Mod 2 = 0})
-            Next i
-            grid.ItemsSource = Persons
+            Next
 
-
-            Dim bands As New List(Of BandItem)()
-            Dim band1 As New BandItem() With {.BandHeader = "Band1"}
+            Bands = New List(Of BandItem)()
+            Dim band1 As BandItem = New BandItem() With {.BandHeader = "Band1"}
             band1.Columns = New List(Of ColumnItem)()
             band1.Columns.Add(New ColumnItem() With {.ColumnFieldName = "Id"})
             band1.Columns.Add(New ColumnItem() With {.ColumnFieldName = "Name"})
-
-            Dim band2 As New BandItem() With {.BandHeader = "Band2"}
+            Dim band2 As BandItem = New BandItem() With {.BandHeader = "Band2"}
             band2.Columns = New List(Of ColumnItem)()
             band2.Columns.Add(New ColumnItem() With {.ColumnFieldName = "Bool"})
-
-            bands.Add(band1)
-            bands.Add(band2)
-            grid.BandsSource = bands
+            Bands.Add(band1)
+            Bands.Add(band2)
+            DataContext = Me
         End Sub
     End Class
 
     Public Class MyGridControl
         Inherits GridControl
+
         Public Shared ReadOnly BandsSourceProperty As DependencyProperty = DependencyProperty.Register("BandsSource", GetType(IList), GetType(MyGridControl), New PropertyMetadata(Nothing, AddressOf OnBandedSourcePropertyChanged))
-        Public Shared ReadOnly BandTemplateProperty As DependencyProperty = DependencyProperty.Register("BandTemplate", GetType(DataTemplate), GetType(MyGridControl), New PropertyMetadata(Nothing))
-        Public Property BandsSource() As IList
+
+        Public Shared ReadOnly BandTemplateProperty As DependencyProperty = DependencyProperty.Register("BandTemplate", GetType(DataTemplate), GetType(MyGridControl), New PropertyMetadata(CType(Nothing, PropertyChangedCallback)))
+
+        Public Property BandsSource As IList
             Get
                 Return CType(GetValue(BandsSourceProperty), IList)
             End Get
+
             Set(ByVal value As IList)
                 SetValue(BandsSourceProperty, value)
             End Set
         End Property
-        Public Property BandTemplate() As DataTemplate
+
+        Public Property BandTemplate As DataTemplate
             Get
                 Return CType(GetValue(BandTemplateProperty), DataTemplate)
             End Get
+
             Set(ByVal value As DataTemplate)
                 SetValue(BandTemplateProperty, value)
             End Set
@@ -65,39 +65,43 @@ Namespace DXGridSample
         Private Shared Sub OnBandedSourcePropertyChanged(ByVal d As DependencyObject, ByVal e As DependencyPropertyChangedEventArgs)
             CType(d, MyGridControl).OnBandedSourceChanged(e)
         End Sub
+
         Private Sub OnBandedSourceChanged(ByVal e As DependencyPropertyChangedEventArgs)
             Bands.Clear()
             For Each b In BandsSource
                 Dim cc As ContentControl = TryCast(BandTemplate.LoadContent(), ContentControl)
-                If cc Is Nothing Then
-                    Continue For
-                End If
+                If cc Is Nothing Then Continue For
                 Dim band As GridControlBand = TryCast(cc.Content, GridControlBand)
                 cc.Content = Nothing
-                If band Is Nothing Then
-                    Continue For
-                End If
+                If band Is Nothing Then Continue For
                 band.DataContext = b
                 Bands.Add(band)
-            Next b
+            Next
         End Sub
     End Class
+
     Public Class MyGridControlBand
         Inherits GridControlBand
+
         Public Shared ReadOnly ColumnsSourceProperty As DependencyProperty = DependencyProperty.Register("ColumnsSource", GetType(IList), GetType(MyGridControlBand), New PropertyMetadata(Nothing, AddressOf OnColumnsSourcePropertyChanged))
-        Public Shared ReadOnly ColumnTemplateProperty As DependencyProperty = DependencyProperty.Register("ColumnTemplate", GetType(DataTemplate), GetType(MyGridControlBand), New PropertyMetadata(Nothing))
-        Public Property ColumnsSource() As IList
+
+        Public Shared ReadOnly ColumnTemplateProperty As DependencyProperty = DependencyProperty.Register("ColumnTemplate", GetType(DataTemplate), GetType(MyGridControlBand), New PropertyMetadata(CType(Nothing, PropertyChangedCallback)))
+
+        Public Property ColumnsSource As IList
             Get
                 Return CType(GetValue(ColumnsSourceProperty), IList)
             End Get
+
             Set(ByVal value As IList)
                 SetValue(ColumnsSourceProperty, value)
             End Set
         End Property
-        Public Property ColumnTemplate() As DataTemplate
+
+        Public Property ColumnTemplate As DataTemplate
             Get
                 Return CType(GetValue(ColumnTemplateProperty), DataTemplate)
             End Get
+
             Set(ByVal value As DataTemplate)
                 SetValue(ColumnTemplateProperty, value)
             End Set
@@ -106,21 +110,18 @@ Namespace DXGridSample
         Private Shared Sub OnColumnsSourcePropertyChanged(ByVal d As DependencyObject, ByVal e As DependencyPropertyChangedEventArgs)
             CType(d, MyGridControlBand).OnColumnsSourceChanged(e)
         End Sub
+
         Private Sub OnColumnsSourceChanged(ByVal e As DependencyPropertyChangedEventArgs)
             Columns.Clear()
             For Each b In ColumnsSource
                 Dim cc As ContentControl = TryCast(ColumnTemplate.LoadContent(), ContentControl)
-                If cc Is Nothing Then
-                    Continue For
-                End If
+                If cc Is Nothing Then Continue For
                 Dim column As GridColumn = TryCast(cc.Content, GridColumn)
                 cc.Content = Nothing
-                If column Is Nothing Then
-                    Continue For
-                End If
+                If column Is Nothing Then Continue For
                 column.DataContext = b
                 Columns.Add(column)
-            Next b
+            Next
         End Sub
     End Class
 End Namespace
--- VB/Model.vb
+++ VB/Model.vb
@@ -1,69 +1,25 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 
 Namespace DXGridSample
-	Public Class Person
-		Private privateId As Integer
-		Public Property Id() As Integer
-			Get
-				Return privateId
-			End Get
-			Set(ByVal value As Integer)
-				privateId = value
-			End Set
-		End Property
-		Private privateName As String
-		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
-		Private privateBool As Boolean
-		Public Property Bool() As Boolean
-			Get
-				Return privateBool
-			End Get
-			Set(ByVal value As Boolean)
-				privateBool = value
-			End Set
-		End Property
-	End Class
 
-	Public Class BandItem
-		Private privateBandHeader As String
-		Public Property BandHeader() As String
-			Get
-				Return privateBandHeader
-			End Get
-			Set(ByVal value As String)
-				privateBandHeader = value
-			End Set
-		End Property
-		Private privateColumns As List(Of ColumnItem)
-		Public Property Columns() As List(Of ColumnItem)
-			Get
-				Return privateColumns
-			End Get
-			Set(ByVal value As List(Of ColumnItem))
-				privateColumns = value
-			End Set
-		End Property
-	End Class
-	Public Class ColumnItem
-		Private privateColumnFieldName As String
-		Public Property ColumnFieldName() As String
-			Get
-				Return privateColumnFieldName
-			End Get
-			Set(ByVal value As String)
-				privateColumnFieldName = value
-			End Set
-		End Property
-	End Class
+    Public Class Person
+
+        Public Property Id As Integer
+
+        Public Property Name As String
+
+        Public Property Bool As Boolean
+    End Class
+
+    Public Class BandItem
+
+        Public Property BandHeader As String
+
+        Public Property Columns As List(Of ColumnItem)
+    End Class
+
+    Public Class ColumnItem
+
+        Public Property ColumnFieldName As String
+    End Class
 End Namespace
