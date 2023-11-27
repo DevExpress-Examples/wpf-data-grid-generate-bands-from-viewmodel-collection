dc17511650b9463d8ab623083e441c00d13dbf6b
DevExpressExampleBot <fakeEmail>
2023-11-27 20:35:51 +0400

    Source auto update [skip ci]

--- VB/GridMVVMBandsSample/BandTemplateSelector.vb
+++ VB/GridMVVMBandsSample/BandTemplateSelector.vb
@@ -1,22 +1,23 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace GridMVVMBandsSample
-	Public Class BandTemplateSelector
-		Inherits DataTemplateSelector
 
-		Public Property SingleColumnBandTemplate() As DataTemplate
-		Public Property MultiColumnBandTemplate() As DataTemplate
+    Public Class BandTemplateSelector
+        Inherits DataTemplateSelector
 
-		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
-			Dim band As Band = TryCast(item, Band)
-			If band Is Nothing Then
-				Return Nothing
-			End If
-			If band.Header = "Position" Then
-				Return SingleColumnBandTemplate
-			End If
-			Return MultiColumnBandTemplate
-		End Function
-	End Class
+        Public Property SingleColumnBandTemplate As DataTemplate
+
+        Public Property MultiColumnBandTemplate As DataTemplate
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+            Dim band As Band = TryCast(item, Band)
+            If band Is Nothing Then Return Nothing
+            If Equals(band.Header, "Position") Then
+                Return SingleColumnBandTemplate
+            End If
+
+            Return MultiColumnBandTemplate
+        End Function
+    End Class
 End Namespace
--- VB/GridMVVMBandsSample/EmployeesViewModel.vb
+++ VB/GridMVVMBandsSample/EmployeesViewModel.vb
@@ -1,51 +1,45 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 
 Namespace GridMVVMBandsSample
-	Public Class ViewModel
-		Inherits ViewModelBase
 
-		Public Sub New()
-			Source = EmployeesDataModel.GetEmployees()
-			Bands = New ObservableCollection(Of Band)() _
-				From {
-					New Band("Personal Info", New ObservableCollection(Of Column)(), New ObservableCollection(Of Band)() _
-						From {
-							New Band("Name", New ObservableCollection(Of Column)() From {
-								New Column(NameOf(Employee.FirstName)),
-								New Column(NameOf(Employee.LastName))
-							}, New ObservableCollection(Of Band)()),
-							New Band("Birthday", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.BirthDate))}, New ObservableCollection(Of Band)())
-						}),
-					New Band("Location", New ObservableCollection(Of Column)() From {
-						New Column(NameOf(Employee.Country)),
-						New Column(NameOf(Employee.City))
-					}, New ObservableCollection(Of Band)()),
-					New Band("Position", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.JobTitle))}, New ObservableCollection(Of Band)())
-				}
-		End Sub
-		Public ReadOnly Property Source() As List(Of Employee)
-		Public ReadOnly Property Bands() As ObservableCollection(Of Band)
-	End Class
-	Public Class Column
-		Inherits BindableBase
+    Public Class ViewModel
+        Inherits ViewModelBase
 
-		Public Sub New(ByVal fieldname As String)
-			Me.FieldName = fieldname
-		End Sub
-		Public ReadOnly Property FieldName() As String
-	End Class
-	Public Class Band
-		Inherits BindableBase
+        Public Sub New()
+            Source = EmployeesDataModel.GetEmployees()
+            Bands = New ObservableCollection(Of Band)() From {New Band("Personal Info", New ObservableCollection(Of Column)(), New ObservableCollection(Of Band)() From {New Band("Name", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.FirstName)), New Column(NameOf(Employee.LastName))}, New ObservableCollection(Of Band)()), New Band("Birthday", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.BirthDate))}, New ObservableCollection(Of Band)())}), New Band("Location", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.Country)), New Column(NameOf(Employee.City))}, New ObservableCollection(Of Band)()), New Band("Position", New ObservableCollection(Of Column)() From {New Column(NameOf(Employee.JobTitle))}, New ObservableCollection(Of Band)())}
+        End Sub
 
-		Public Sub New(ByVal header As String, ByVal childcolumns As ObservableCollection(Of Column), ByVal childbands As ObservableCollection(Of Band))
-			Me.Header = header
-			Me.ChildColumns = childcolumns
-			Me.ChildBands = childbands
-		End Sub
-		Public ReadOnly Property Header() As String
-		Public ReadOnly Property ChildColumns() As ObservableCollection(Of Column)
-		Public ReadOnly Property ChildBands() As ObservableCollection(Of Band)
-	End Class
+        Public ReadOnly Property Source As List(Of Employee)
+
+        Public ReadOnly Property Bands As ObservableCollection(Of Band)
+    End Class
+
+    Public Class Column
+        Inherits BindableBase
+
+        Public Sub New(ByVal fieldname As String)
+            Me.FieldName = fieldname
+        End Sub
+
+        Public ReadOnly Property FieldName As String
+    End Class
+
+    Public Class Band
+        Inherits BindableBase
+
+        Public Sub New(ByVal header As String, ByVal childcolumns As ObservableCollection(Of Column), ByVal childbands As ObservableCollection(Of Band))
+            Me.Header = header
+            Me.ChildColumns = childcolumns
+            Me.ChildBands = childbands
+        End Sub
+
+        Public ReadOnly Property Header As String
+
+        Public ReadOnly Property ChildColumns As ObservableCollection(Of Column)
+
+        Public ReadOnly Property ChildBands As ObservableCollection(Of Band)
+    End Class
 End Namespace
